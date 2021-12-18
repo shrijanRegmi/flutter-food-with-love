2d161b36aa7f20e80b004343709cdc501b38a784
Shrijan Regmi <ilyyhs9@gmail.com>
2021-12-18 11:45:00 +0545

    text alignment added

--- lib/src/widgets/food_with_love_text.dart
+++ lib/src/widgets/food_with_love_text.dart
@@ -5,27 +5,54 @@ class FoodWithLoveText extends StatelessWidget {
   final String value;
   final TextStyle defaultStyle;
   final TextStyle? style;
+  final TextAlign? textAlign;
 
-  const FoodWithLoveText.heading1(this.value, {this.style})
-      : defaultStyle = headingText1;
-  const FoodWithLoveText.heading2(this.value, {this.style})
-      : defaultStyle = headingText2;
-  const FoodWithLoveText.heading3(this.value, {this.style})
-      : defaultStyle = headingText3;
-  const FoodWithLoveText.heading4(this.value, {this.style})
-      : defaultStyle = headingText4;
-  const FoodWithLoveText.heading5(this.value, {this.style})
-      : defaultStyle = headingText5;
-  const FoodWithLoveText.heading6(this.value, {this.style})
-      : defaultStyle = headingText6;
-  const FoodWithLoveText.body(this.value, {this.style})
-      : defaultStyle = bodyText;
+  const FoodWithLoveText.heading1(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = headingText1;
+
+  const FoodWithLoveText.heading2(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = headingText2;
+
+  const FoodWithLoveText.heading3(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = headingText3;
+
+  const FoodWithLoveText.heading4(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = headingText4;
+
+  const FoodWithLoveText.heading5(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = headingText5;
+  const FoodWithLoveText.heading6(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = headingText6;
+  const FoodWithLoveText.body(
+    this.value, {
+    this.style,
+    this.textAlign = TextAlign.start,
+  }) : defaultStyle = bodyText;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       value,
       style: defaultStyle.merge(style),
+      textAlign: textAlign,
     );
   }
 }
